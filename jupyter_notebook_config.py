fa2e047c2cdd68952a23af844cd43b353d691647
Yorushika <yorushika@outlook.com>
2018-11-22 17:34:02 +0800

    sort out Dockerfiles

--- jupyter_notebook_config.py
+++
@@ -1,5 +0,0 @@
-c.NotebookApp.ip='*'
-c.NotebookApp.open_browser = False
-c.NotebookApp.port = 8888
-c.NotebookApp.base_project_url = '/root/data'
-c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "frame-ancestors 'self' *" } }
